bbb4f270f21815cb4d62c85b1af5147e7d895184
poma123 <vhvvvhhh@gmail.com>
2020-06-02 00:56:07 +0200

    Updated to 1.15

--- src/main/java/plus/crates/csgo/CSGOOpener.java
+++ src/main/java/plus/crates/csgo/CSGOOpener.java
@@ -8,19 +8,16 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import plus.crates.Crates.Crate;
-import plus.crates.Crates.KeyCrate;
-import plus.crates.Crates.Winning;
+import org.bukkit.scheduler.BukkitTask;
+import plus.crates.Crate;
 import plus.crates.Opener.Opener;
+import plus.crates.Winning;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 public class CSGOOpener extends Opener {
-	private HashMap<UUID, Integer> tasks = new HashMap<>();
+	//private HashMap<UUID, BukkitTask> tasks = new HashMap<>();
 	private HashMap<UUID, Inventory> guis = new HashMap<>();
 	private int length = 10;
 
@@ -58,12 +55,14 @@ public class CSGOOpener extends Opener {
 		final int slowSpeedTime = maxTimeTicks / 20;
 		final int fastSpeedTime = (maxTimeTicks / 10) * 9;
 		final ArrayList<Winning> last5Winnings = new ArrayList<>();
-		tasks.put(player.getUniqueId(), Bukkit.getScheduler().runTaskTimerAsynchronously(getPlugin(), new BukkitRunnable() {
+		/*tasks.put(player.getUniqueId(), */
+		new BukkitRunnable() {
 			public void run() {
 				if (!player.isOnline()) {
 					finish(player);
 					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "crate key " + player.getName() + " " + crate.getName() + " 1");
-					Bukkit.getScheduler().cancelTask(tasks.get(player.getUniqueId()));
+					//Bukkit.getScheduler().cancelTask(tasks.get(player.getUniqueId()));
+					this.cancel();
 					return;
 				}
 				if ((timer[0] > fastSpeedTime || timer[0] < slowSpeedTime) && (timer[0] & 1) == 0) {
@@ -74,7 +73,7 @@ public class CSGOOpener extends Opener {
 				while (i < 27) {
 
 					if (i == 4 || i == 22) {
-						ItemStack torch = new ItemStack(Material.REDSTONE_TORCH_ON);
+						ItemStack torch = new ItemStack(Material.REDSTONE_TORCH);
 						ItemMeta itemMeta = torch.getItemMeta();
 						itemMeta.setDisplayName(ChatColor.GREEN + " ");
 						torch.setItemMeta(itemMeta);
@@ -87,7 +86,8 @@ public class CSGOOpener extends Opener {
 					if (i >= 10 && i <= 16) {
 
 						if (i == 16) {
-							winning = crate.getRandomWinning();
+
+							winning = getWinning(crate);//crate.getRandomWinning();
 							if (last5Winnings.size() == 3)
 								last5Winnings.remove(0);
 							last5Winnings.add(winning);
@@ -95,12 +95,12 @@ public class CSGOOpener extends Opener {
 						} else if (winGUI.getItem(i + 1) != null) {
 							winGUI.setItem(i, winGUI.getItem(i + 1));
 						}
-
 						if (i == 13) {
 
 							if (timer[0] >= maxTimeTicks) {
 								winning = last5Winnings.get(0);
-								crate.handleWin(player, winning);
+								winning.runWin(player);
+								//crate.handleWin(player, winning);
 							}
 
 						}
@@ -108,29 +108,34 @@ public class CSGOOpener extends Opener {
 						i++;
 						continue;
 					}
-					ItemStack itemStack = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) new Random().nextInt(15));
+
+					List<String> l = Arrays.asList("WHITE", "ORANGE", "MAGENTA", "LIGHT_BLUE", "YELLOW", "LIME", "PINK", "GRAY", "LIGHT_GRAY", "CYAN", "PURPLE", "BLUE", "BROWN", "GREEN", "RED", "BLACK");
+
+
+
+					ItemStack itemStack = new ItemStack(Material.getMaterial(l.get(new Random().nextInt(15)) + "_STAINED_GLASS_PANE"), 1);
 					ItemMeta itemMeta = itemStack.getItemMeta();
 					if (timer[0] >= maxTimeTicks) {
 						itemMeta.setDisplayName(ChatColor.RESET + "Winner!");
 					} else {
-						Sound sound;
+						/*Sound sound;
 						try {
-							sound = Sound.valueOf("NOTE_PIANO");
+							sound = Sound.BLOCK_NOTE_BLOCK_PLING;
 						} catch (Exception e) {
 							try {
-								sound = Sound.valueOf("BLOCK_NOTE_HARP");
+								sound = Sound.BLOCK_NOTE_BLOCK_HARP;
 							} catch (Exception ee) {
 								return; // This should never happen!
 							}
 						}
 						final Sound finalSound = sound;
-						Bukkit.getScheduler().runTask(getPlugin(), new Runnable() {
+						new BukkitRunnable() {
 							@Override
 							public void run() {
 								if (player.getOpenInventory().getTitle() != null && player.getOpenInventory().getTitle().contains(" Win"))
 									player.playSound(player.getLocation(), finalSound, (float) 0.2, 2);
 							}
-						});
+						}.runTask(getPlugin());*/
 						itemMeta.setDisplayName(ChatColor.RESET + "Rolling...");
 					}
 					itemStack.setItemMeta(itemMeta);
@@ -139,17 +144,15 @@ public class CSGOOpener extends Opener {
 				}
 				if (timer[0] >= maxTimeTicks) {
 					finish(player);
-					Bukkit.getScheduler().cancelTask(tasks.get(player.getUniqueId()));
+					//Bukkit.getScheduler().cancelTask(tasks.get(player.getUniqueId()));
+					this.cancel();
 					return;
 				}
 				timer[0]++;
 			}
-		}, 0L, 2L).getTaskId());
+		}.runTaskTimerAsynchronously( getPlugin(), 0L, 2L);
 	}
 
-	@Override
-	public boolean doesSupport(Crate crate) {
-		return crate instanceof KeyCrate;
-	}
+
 
 }
